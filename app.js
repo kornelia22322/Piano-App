b7044f3b18be2813f9cc856561c4103eeb65cc30
korneliarohullko <korneliarohulko@gmail.com>
2018-02-24 03:05:24 +0100

    Change process.env.port

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var musicController = require('./controllers/musicController');
+var port = process.env.PORT || 3003;
 
 var app = express();
 app.use(express.static('./public'));
 
 musicController(app);
-app.listen(3003);
+app.listen(port);
